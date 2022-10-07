5d21a070931b588506f8e01782b6c92f6ad89299
Matteo Susanna <matteo2784@gmail.com>
2022-10-07 15:35:22 +0200

    push

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -3,13 +3,30 @@
 @section('title', 'Log-in pagina principale')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Log effettuato con successo</div>
-            </div>
-        </div>
+    <div class="container">
+        <table class="table table-dark table-striped">
+            <thead>
+                <tr>
+                <th scope="col">#</th>
+                <th scope="col">Nome</th>
+                <th scope="col">Slug</th>
+                <th scope="col">Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                    @foreach ($posts as $post)
+                        <tr>
+                            <th scope="row">{{$post->id}}</th>
+                            <td>{{$post->name}}</td>
+                            <td>{{$post->slug}}</td>
+                            <td>
+                                <a href="#" class="btn btn-primary">Vedi</a>
+                                <a href="#" class="btn btn-warning">Modifica</a>
+                            </td>
+                        </tr>
+                    @endforeach
+            </tbody>
+
+        </table>
     </div>
-</div>
 @endsection
