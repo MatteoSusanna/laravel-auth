5adbb5b8e9458f3f1f86c38fc9464c551395f721
Matteo Susanna <matteo2784@gmail.com>
2022-10-07 15:07:26 +0200

    push inizio CRUD

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::resource('posts', 'PostController');
 
 Auth::routes();
 
@@ -26,3 +24,8 @@ Route::middleware('auth')
     ->group(function(){
         Route::get('/', 'homecontroller@index')->name('home');
     });
+
+
+Route::get('{any?}', function(){
+    return view('guest.home');
+})->where('any', '.*');
